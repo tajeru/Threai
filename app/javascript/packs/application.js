f4f9cde3ad217aa6865b60360ebbaefa42472d17
tajeru <tajeru47@gmail.com>
2024-05-27 22:42:07 +0900

    try again changed webpack.config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,9 @@
+console.log("Hello, Webpack!");
+
+import "@hotwired/turbo-rails"
 import './custom';
+import "controllers"
 import 'bootstrap'
-import Rails from "@rails/ujs";
-Rails.start();
+import * as bootstrap from "bootstrap"
+import Rails from "@rails/ujs"
+Rails.start()
---
+++ webapck.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  entry: './app/javascript/application.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/packs'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000,
+  },
+};
